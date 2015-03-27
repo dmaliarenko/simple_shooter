fe955f3c117ef08daa8ec0dda8b0e378684787f9
Dmitriy Malyarenko <demitriy.s@gmail.com>
2015-03-27 10:44:05 +0200

    modified public/js/game.js

--- public/js/game.js
+++ public/js/game.js
@@ -34,8 +34,8 @@ function init() {
 	localPlayer = new Player(startX, startY);
 
 	// Initialise socket connection
-	socket = io.connect("http://localhost", {port: 1253, transports: ["websocket"]});
-	
+	//socket = io.connect("http://localhost", {port: 1253, transports: ["websocket"]});
+	socket = io();
 
 	// Initialise remote players array
 	remotePlayers = [];
